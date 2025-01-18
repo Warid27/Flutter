179b62b2e2745eb813b2d4c90fbf71554567aa15
Warid27 <mohamadsinatr3@gmail.com>
2025-01-18 20:57:15 +0700

    first commit

---
+++ Flutter Projects/MyFlutter/bucketlist/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\Unity\\SDK\\Flutter_SDK\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\Unity\\Pelatihan-main\\Udemy\\Flutter\\Flutter Projects\\MyFlutter\\bucketlist" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\Unity\\SDK\\Flutter_SDK\\flutter"
+  "PROJECT_DIR=E:\\Unity\\Pelatihan-main\\Udemy\\Flutter\\Flutter Projects\\MyFlutter\\bucketlist"
+  "FLUTTER_ROOT=E:\\Unity\\SDK\\Flutter_SDK\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\Unity\\Pelatihan-main\\Udemy\\Flutter\\Flutter Projects\\MyFlutter\\bucketlist\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\Unity\\Pelatihan-main\\Udemy\\Flutter\\Flutter Projects\\MyFlutter\\bucketlist"
+  "FLUTTER_TARGET=E:\\Unity\\Pelatihan-main\\Udemy\\Flutter\\Flutter Projects\\MyFlutter\\bucketlist\\lib\\main.dart"
+  "DART_DEFINES=TVlBUFBfRU5WSVJPTk1FTlQ9REVW"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\Unity\\Pelatihan-main\\Udemy\\Flutter\\Flutter Projects\\MyFlutter\\bucketlist\\.dart_tool\\package_config.json"
+)
